3201f3f6dd27f50a54d16d83bf94ac61aa4b7aeb
alex <pluscda@gmail.com>
2021-03-24 15:10:00 +0800

    g

--- src/locales/lang/zh_CN.ts
+++ src/locales/lang/zh_CN.ts
@@ -1,6 +1,6 @@
 import { genMessage } from '../helper';
-import antdLocale from 'ant-design-vue/es/locale/zh_CN';
-import momentLocale from 'moment/dist/locale/zh-cn';
+import antdLocale from 'ant-design-vue/es/locale/zh_TW';
+import momentLocale from 'moment/dist/locale/zh-tw';
 
 const modules = import.meta.globEager('./zh_CN/**/*.ts');
 export default {
