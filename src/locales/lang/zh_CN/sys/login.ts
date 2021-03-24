7f7645927c172d23b877682493610a048a9a6292
alex <pluscda@gmail.com>
2021-03-24 13:33:36 +0800

    g

--- src/locales/lang/zh_CN/sys/login.ts
+++ src/locales/lang/zh_CN/sys/login.ts
@@ -7,7 +7,7 @@ export default {
   forgetFormTitle: '重置密碼',
 
   signInTitle: '基層醫院訊息後臺管理系統',
-  signInDesc: '輸入您的個人詳細信息開始使用！',
+  signInDesc: '登錄您的個人詳細信息開始使用！',
   policy: '我同意Datacom隱私政策',
   scanSign: `掃碼後點擊"確認"，即可完成登錄`,
 
